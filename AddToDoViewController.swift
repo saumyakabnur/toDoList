a46a885bf28219b711bacb78c6d0d33cd6523901
Saumya Kabnur <saumyakabnur@gmail.com>
2022-06-30 15:20:53 -0500

    hi

---
+++ AddToDoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddToDoViewController.swift
+//  toDoList
+//
+//  Created by Scholar on 6/27/22.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    var previousVC = ToDoTableViewController()
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+
+         if let titleText = titleTextField.text {
+           toDo.name = titleText
+           toDo.important = importantSwitch.isOn
+         }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ toDoList/ToDo.swift
@@ -0,0 +1,11 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Scholar on 6/24/22.
+//
+import UIKit
+class ToDo{
+    var name = ""
+    var important = false
+}
